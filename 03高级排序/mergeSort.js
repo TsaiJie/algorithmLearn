7d7279a6ed6ccb957b26a61a523d238e9d7c0166
tsai <mengjietsai@163.com>
2021-04-05 10:32:31 +0800

    目录名称修改

--- 03高级排序/mergeSort.js
+++ 03高级排序/mergeSort.js
@@ -38,6 +38,18 @@ const mergeSort = (arr) => {
   }
 };
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+}
 const arr = [1, 0, 10, 100, 9, 99, 88, 77, 44, 34, 23, 54];
-mergeSort(arr);
-console.log(arr);
+const arr2 = []
+
+for (let index = 0; index < 10000000; index++) {
+   arr2.push(getRandomIntInclusive(0, 1000000))
+}
+console.time()
+mergeSort(arr2);
+console.timeEnd()
+console.log(arr2)
